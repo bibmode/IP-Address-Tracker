dd2c92946a3cf61ca149fe359acba397fb148d1c
Genevieve <vievenavales29@gmail.com>
2021-08-30 23:36:19 +0800

    input validation

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const inputField = document.querySelector(".input__field");
+const cardContents = document.querySelectorAll(".card-content");
+
 ////////////////////////////////////////
 //getting data from API
 let lat, lng, map;
@@ -8,7 +11,13 @@ const fetchData = function (ipUser = "") {
   fetch(
     `https://geo.ipify.org/api/v1?apiKey=at_dwa8sZ2rLXbOyLCd7fZQHASemVGTR&ipAddress=${ipUser}`
   )
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("IP address not valid. Try again!");
+      }
+
+      return response.json();
+    })
     .then((data) => {
       lat = data.location.lat;
       lng = data.location.lng;
@@ -24,7 +33,35 @@ const fetchData = function (ipUser = "") {
 
       //adding map to UI
       showLocationMap(lat, lng);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      errorHandler();
     });
+};
+
+////////////////////////////////////////
+//error function
+const errorHandler = function () {
+  inputField.classList.add("error-theme");
+  inputField.placeholder = "Insert a valid IP address!";
+
+  cardContents.forEach((content) => {
+    content.innerText = "";
+  });
+};
+
+////////////////////////////////////////
+//generate map
+const generateMap = function (lat, lng) {
+  map = L.map("map").setView([lat, lng], 17);
+
+  L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }).addTo(map);
+
+  L.marker([lat, lng]).addTo(map).bindPopup("Here it is!").openPopup();
 };
 
 ////////////////////////////////////////
@@ -32,24 +69,9 @@ const fetchData = function (ipUser = "") {
 const showLocationMap = function (lat, lng) {
   if (map != null) {
     map.remove();
-
-    map = L.map("map").setView([lat, lng], 17);
-
-    L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
-      maxZoom: 20,
-      subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }).addTo(map);
-
-    L.marker([lat, lng]).addTo(map).bindPopup("Here it is!").openPopup();
+    generateMap(lat, lng);
   } else {
-    map = L.map("map").setView([lat, lng], 17);
-
-    L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
-      maxZoom: 20,
-      subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }).addTo(map);
-
-    L.marker([lat, lng]).addTo(map).bindPopup("Here it is!").openPopup();
+    generateMap(lat, lng);
   }
 };
 
@@ -66,7 +88,7 @@ const cardDetails = function (ip, city, country, geoId, timezone, isp) {
   document.querySelector("#city").textContent = `${city}, `;
   document.querySelector("#country").textContent = `${country}`;
   document.querySelector("#geoId").textContent = `${geoId}`;
-  document.querySelector("#timezone").textContent = `${timezone}`;
+  document.querySelector("#timezone").textContent = `UTC ${timezone}`;
   document.querySelector("#isp").textContent = `${isp}`;
 };
 
@@ -80,7 +102,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 //input button click
 document.querySelector(".input__btn").addEventListener("click", () => {
-  const userInput = document.querySelector(".input__field").value;
+  //let userInput = document.querySelector(".input__field");
 
-  if (userInput.trim() !== "") fetchData(userInput);
+  /* if (userInput.value.trim() !== "") {
+    fetchData(userInput.value);
+    userInput.value = "";
+  } */
+  if (inputField.value.trim() !== "") {
+    fetchData(inputField.value);
+    inputField.value = "";
+  }
+});
+
+//user click input
+document.querySelector(".input__field").addEventListener("click", () => {
+  inputField.classList.remove("error-theme");
+  inputField.placeholder = "Search for any IP address or domain";
 });
