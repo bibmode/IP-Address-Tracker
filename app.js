e94327373a264f93fe5b957287f07eea86f6c3a2
Genevieve <vievenavales29@gmail.com>
2021-08-30 20:31:52 +0800

    change tile layer to google maps

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 "use strict";
 
-var map = L.map("map").setView([51.5, -0.09], 16);
+////////////////////////////////////////
+//open user location on map as user loads
+let lat, lng;
 
-L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png").addTo(map);
+fetch(`https://geo.ipify.org/api/v1?apiKey=at_dwa8sZ2rLXbOyLCd7fZQHASemVGTR&`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    lat = data.location.lat;
+    lng = data.location.lng;
+    console.log(lat, lng);
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+    //adding map to UI
+    showLocationMap(lat, lng);
+  });
+
+////////////////////////////////////////
+//adding map to UI
+const showLocationMap = function (lat, lng) {
+  var map = L.map("map").setView([lat, lng], 17);
+
+  L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }).addTo(map);
+
+  L.marker([lat, lng]).addTo(map).bindPopup("Here it is!").openPopup();
+};
+
+////////////////////////////////////////
+//adding information on cards
